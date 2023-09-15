99f0f49dfa424e11e785efc9dcc9bab512bf4dd8
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-15 15:13:58 +0200

    fix: navbar search

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,23 +20,26 @@ export default {
   <header class="d-flex justify-content-between p-2">
     <div class="logo"></div>
     <div class="input-group mb-3 w-25">
-      <form @submit.prevent="$emit(`form-submit`, textSearch)">
-        <input
-          v-model="textSearch"
-          type="text"
-          class="form-control"
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="button-addon2"
-        />
-        <button
-          class="btn btn-outline-secondary"
-          type="button"
-          id="button-addon2"
-        >
-          Button
-        </button>
-      </form>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <form
+            @submit.prevent="$emit(`form-submit`, textSearch)"
+            class="d-flex"
+            role="search"
+          >
+            <input
+              v-model="textSearch"
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     </div>
   </header>
 </template>
